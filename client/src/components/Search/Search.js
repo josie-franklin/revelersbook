1ce1cddbfe0228c4e006096256b84578d1ff5fb0
dolcebasstrombone <dolcebasstrombone@gmail.com>
2023-01-14 21:14:06 -0500

    adjusted styles for the search page

--- client/src/components/Search/Search.js
+++ client/src/components/Search/Search.js
@@ -1,5 +1,6 @@
 import React, { useState, useContext, useEffect } from "react";
 // import { Link } from "react-router-dom";
+import { styled } from "@mui/system";
 import SearchBar from "./SearchBar";
 import Results from "./SearchResults";
 import Box from "@mui/material/Box";
@@ -8,15 +9,25 @@ import Stack from "@mui/material/Stack";
 import Typography from "@mui/material/Typography";
 // import TextField from "@mui/material/TextField";
 
+const HeaderText = styled(Typography)(({ theme }) => ({
+  fontWeight: "700",
+  fontSize: "2.5rem",
+  textAlign: "center",
+  [theme.breakpoints.down("md")]: {
+  fontSize: "2rem",
+  },
+  [theme.breakpoints.down("sm")]: {
+    fontSize: "1.5rem",
+    },
+}));
+
 const SearchPage = () => {
   useEffect(() => {}, []);
 
   return (
     <Box className="flex-column align-center">
-      <Stack spacing={2} alignItems="center">
-        <Typography variant="h1" sx={{ fontSize: "3rem" }}>
-          NC Revelers Orchestra Book
-        </Typography>
+      <Stack spacing={2} alignItems="center" sx={{ width: "100%" }}>
+        <HeaderText variant="h1">NC Revelers Orchestra Book</HeaderText>
 
         <SearchBar />
 
--- client/src/components/Search/SearchResults.js
+++ client/src/components/Search/SearchResults.js
@@ -1,8 +1,11 @@
 import React, { useContext, useEffect } from "react";
 // import { Link } from "react-router-dom";
 import { ChartContext } from "../../context/ChartContext";
+import { styled } from "@mui/system";
 import Box from "@mui/material/Box";
+import Paper from "@mui/material/Paper";
 import Grid from "@mui/material/Grid";
+import Stack from "@mui/material/Stack";
 import Typography from "@mui/material/Typography";
 import Table from "@mui/material/Table";
 import TableBody from "@mui/material/TableBody";
@@ -10,6 +13,24 @@ import TableCell from "@mui/material/TableCell";
 import TableContainer from "@mui/material/TableContainer";
 import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
+
+const Row = styled(Box)(() => ({
+  width: "100%",
+  padding: "10px",
+}));
+
+const Text = styled(Typography)(({ theme }) => ({
+  [theme.breakpoints.down("sm")]: {
+    fontSize: "0.8rem",
+  },
+  [theme.breakpoints.down("xs")]: {
+    fontSize: "0.5rem",
+  },
+}));
+
+const BoldText = styled(Text)(() => ({
+  fontWeight: "700",
+}));
 
 const Results = () => {
   const { chartObj, setChartObj, getAllCharts } = useContext(ChartContext);
@@ -21,32 +42,49 @@ const Results = () => {
 
   const renderCharts = () => {
     let render = chartObj.length ? (
-      <TableContainer>
-        <Table>
-          <TableHead>
-            <TableRow>
-              <TableCell>Book #</TableCell>
-              <TableCell>Title</TableCell>
-              <TableCell>Composer</TableCell>
-              <TableCell>Arranger</TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {chartObj.map((chart) => (
-              <TableRow key={chart.id}>
-                <TableCell>{chart.bookNumber}</TableCell>
-                <TableCell>{chart.title}</TableCell>
-                <TableCell>{chart.composer}</TableCell>
-                <TableCell>{chart.arranger}</TableCell>
-              </TableRow>
-            ))}
-          </TableBody>
-        </Table>
-      </TableContainer>
+      <Stack>
+        <Row sx={{ borderBottom: "1px solid grey" }}>
+          <Grid container spacing={2}>
+            <Grid item xs={1.5}>
+              <BoldText>Book#</BoldText>
+            </Grid>
+            <Grid item xs={4.5}>
+              <BoldText>Title</BoldText>
+            </Grid>
+            <Grid item xs={3}>
+              <BoldText>Composer</BoldText>
+            </Grid>
+            <Grid item xs={3}>
+              <BoldText>Arranger</BoldText>
+            </Grid>
+          </Grid>
+        </Row>
+        {chartObj.map((chart, i) => (
+          <Row
+            key={chart.id}
+            sx={{ backgroundColor: i % 2 === 0 ? "#d9d9d9" : "" }}
+          >
+            <Grid container spacing={2}>
+              <Grid item xs={1.5}>
+                <Text>{chart.bookNumber}</Text>
+              </Grid>
+              <Grid item xs={4.5}>
+                <Text>{chart.title}</Text>
+              </Grid>
+              <Grid item xs={3}>
+                <Text>{chart.composer || "---"}</Text>
+              </Grid>
+              <Grid item xs={3}>
+                <Text>{chart.arranger || "---"}</Text>
+              </Grid>
+            </Grid>
+          </Row>
+        ))}
+      </Stack>
     ) : (
       <Typography>No charts were found.</Typography>
     );
-    return <div>{render}</div>;
+    return <Box>{render}</Box>;
   };
 
   let contents = !chartObj ? <p>Loading...</p> : renderCharts();
@@ -57,7 +95,7 @@ const Results = () => {
     }
   }, [chartObj]);
 
-  return <div>{contents}</div>;
+  return <Box sx={{ width: "100%" }}>{contents}</Box>;
 };
 
 export default Results;
